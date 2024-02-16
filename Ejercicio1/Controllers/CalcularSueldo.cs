061df71c0ee857eee6f68d958efb61f6fd7c84b7
prueba <mabel.fajardo@sistemasgyg.com.co>
2024-02-16 14:13:19 -0500

    API con dos ejercicios en ASP .Net Core

---
+++ Ejercicio1/Controllers/CalcularSueldo.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ejercicio1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class CalcularSueldo : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Calculadora([Required] string nombre, [Required] long horasDeTrabajo, [Required] decimal valorPagoHora)
+        {
+            if (string.IsNullOrEmpty (nombre) || horasDeTrabajo <= 0 || valorPagoHora <= 0)
+            {
+                return BadRequest("Por favor ingresa valores válidos para el nombre, horas de trabajo y valor del pago por hora.");
+            }
+
+            decimal sueldo = horasDeTrabajo * valorPagoHora;
+
+            return Ok($"El sueldo de {nombre} es: {sueldo}");
+        }
+    }
+}
---
+++ Ejercicio1/Controllers/EjerParImpar.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ejercicio1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EjerParImpar : ControllerBase
+    {
+        [HttpGet("{numer}")]
+        public IActionResult ProcesarNumero([FromRoute] long numer)
+        {
+            string result = (numer % 2 == 0) ? "par" : "impar";
+
+            return Ok($"El número {numer} es {result}.");
+        }
+    }
+}
